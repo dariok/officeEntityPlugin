26b754aef8e54c5a663561b09bf7a5e9216ea4fb
Dario Kampkaspar <Dario.Kampkaspar@oeaw.ac.at>
2017-12-15 11:44:14 +0100

    This version works

--- entity.js
+++ entity.js
@@ -1,34 +1,40 @@
-Office.initialize = function (reason) {
-    // Checks for the DOM to load using the jQuery ready function.
-    $(document).ready(function () {
-        $('#s0').html( reason );
-    });
+Office.initialize = function ( reason ) {
+	// Checks for the DOM to load using the jQuery ready function.
+	$( document ).ready(
+		function () {
+		// void
+		}
+	);
 };
 
 function insertLink ( link ) {
-        Office.context.document.getSelectedDataAsync(
-                Office.CoercionType.Html,
-                function (result) {
-                        if (result.status == "succeeded") {
-                                // Get the OOXML returned from the getSelectedDataAsync call
-                                var ret = $( result.value );
-                                var par = ret.find('.Paragraph');
-                                var selectedText = par.text();
-                                var elem = ret.find('.TextRun').first();
-                                elem.html(`<a href="${link}">${selectedText}</a>`);
-                                console.log( wrap );
-
-                                Office.context.document.setSelectedDataAsync(
-                                        par.html(),
-                                        { coercionType: Office.CoercionType.Html },
-                                        function (asyncResult) {
-                                                if (asyncResult.status == "failed") {
-                                                        console.log(asyncResult.error.message);
-                                                        console.log(asyncResult.error.name);
-                                                }
-                                        }
-                                );
-                        }
-                }
-        );
+	// get the selected data as HTML
+	Office.context.document.getSelectedDataAsync(
+		Office.CoercionType.Html,
+		function ( result ) {
+			if ( result.status == "succeeded" ) {
+				// the selected data as HTML, i.e. incl. formatting
+				var res = $( result.value );
+				
+				// there should be exactly one p in there that contains the selected text
+				// it is a p due to schema constraints
+				var ret = res.find( 'p' );
+				
+				// the selected text is inside spans that correspond to Word's "runs" (w:r)
+				// @style contains all styling information and needs to be kept
+				var text = ret.html();
+				
+				//we simply wrap the contents of that p in an html:a and write it back
+				var ref = '<a href="' + link + '">' + text + '</a>';
+				
+				Office.context.document.setSelectedDataAsync(
+					ref,
+					{ coercionType: Office.CoercionType.Html },
+					function ( asyncResult ) {
+						console.log( asyncResult.status );
+					}
+				);
+			}
+		}
+	);
 }
