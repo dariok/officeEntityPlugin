c0ee7728583ea41c02ecfbcf5eaeb541ed91f7c1
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-11-05 20:43:10 +0100

    Form-bezogene JS in eigene Datei auslagern

---
+++ form.js
@@ -0,0 +1,27 @@
+$('#form_people').select2({
+	minimumInputLength: 2,
+	escapeMarkup: function(markup) {
+		return markup;
+	},
+	templateResult: function(data) {
+		return data.text;
+	},
+	templateSelection: function(data) {
+		return data.text;
+	},
+	ajax: {
+		url: function() { return 'https://apisdev.acdh.oeaw.ac.at/entities/autocomplete/' + $('#type').val() + '/' },
+		dataType: 'json',
+		processResults: function (data) {
+			return { results: process(data) };
+		}
+	}
+});
+
+$('body').on('submit', 'form#person-form', function(event) {
+	event.stopPropagation();
+	event.preventDefault();
+	var link = $('select#form_people').val();
+	console.log( link );
+	insertLink ( link );
+});
