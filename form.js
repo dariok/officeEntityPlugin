dfac04beb1d2529017b8160bbcde599f820c160a
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-11-06 13:56:58 +0100

    load types, query, processing
    - move select2 back to form.js
    - populate #type from configuration
    - set x-access-token header if token present in config
    - assemble query url from config
    - use local function to assemble query string
    
    Cf. #3

--- form.js
+++ form.js
@@ -1,10 +1,47 @@
 var conf =[];
-$.getJSON('config.json', function (data) {
-	conf = data.data;
-	
-	Object.keys(conf.types).forEach(function(key) {
-		$('#type').append($('<option>', { text:conf.types[key], value: key }));
-	});
+$.ajax({
+	dataType: "json",
+	url: "config.json",
+	success: function (data) {
+		conf = data.data;
+		
+		Object.keys(conf.types).forEach(function(key) {
+			$('#type').append($('<option>', { text:conf.types[key], value: key }));
+		});
+		
+		var ajaxConf;
+		if (conf.token !== '' && conf.token !== 'undefined') {
+			ajaxConf = {
+					"x-access-token": conf.token
+				}
+		} else {
+			ajaxConf = { }
+		}
+		
+		$('#search-entity').select2({
+			minimumInputLength: 2,
+			escapeMarkup: function (markup) {
+				return markup;
+			},
+			templateResult: function (data) {
+				return data.text;
+			},
+			templateSelection: function (data) {
+				return data.text;
+			},
+			ajax: {
+				url: function () { return conf.server + $('#type').val() + conf.suffix },
+				data: function (params) { return queryString (conf, params) },
+				headers: ajaxConf,
+				dataType: 'json',
+				processResults: function (data) {
+					return {
+						results: process(data)
+					};
+				}
+			}
+		});
+	}
 });
 
 $('body').on('submit', 'form#person-form', function (event) {
