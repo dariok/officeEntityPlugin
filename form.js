746916e1f5510cc3bde28b112a85c992c8f3cd00
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2018-11-06 10:58:31 +0100

    get types options from config
    Cf. #3

--- form.js
+++ form.js
@@ -1,6 +1,10 @@
 var conf =[];
 $.getJSON('config.json', function (data) {
 	conf = data.data;
+	
+	Object.keys(conf.types).forEach(function(key) {
+		$('#type').append($('<option>', { text:conf.types[key], value: key }));
+	});
 });
 
 $('body').on('submit', 'form#person-form', function (event) {
