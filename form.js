88f5d90f25b1dfecae4f2a7f6e674940b046c938
Dario Kampkaspar <dario.kampkaspar@oeaw.ac.at>
2021-10-29 14:17:25 +0200

    read "strings" prop only if set in config

--- form.js
+++ form.js
@@ -1,3 +1,5 @@
+/* jshint browser: true */
+
 var conf =[];
 $.ajax({
    dataType: "json",
@@ -36,7 +38,7 @@ $.ajax({
          fieldset.append($('<input type="submit">'));
          i++;
       });
-	}
+   }
       $("#app-form input").on('click', function (event) {
          event.stopPropagation();
          event.preventDefault();
@@ -48,44 +50,44 @@ $.ajax({
 });
 
 function setConf() {
-      var ajaxConf;
-      if (conf.authMode == "static token" && conf.token !== '' && conf.token !== 'undefined') {
-         ajaxConf = {
-               "x-access-token": conf.token
-            }
-      } else if (conf.authMode == "dynamic token") {
-         var local = localStorage.getItem("oepDynamicToken");
-         console.log(local);
-         ajaxConf = {
-            "x-access-token": local
+   var ajaxConf;
+   if (conf.authMode == "static token" && conf.token !== '' && conf.token !== 'undefined') {
+      ajaxConf = {
+            "x-access-token": conf.token
          }
-      } else {
-         ajaxConf = { }
+   } else if (conf.authMode == "dynamic token") {
+      var local = localStorage.getItem("oepDynamicToken");
+      console.log(local);
+      ajaxConf = {
+         "x-access-token": local
       }
-      
-      $('#search-entity').select2({
-         minimumInputLength: 2,
-         escapeMarkup: function (markup) {
-            return markup;
+   } else {
+      ajaxConf = { }
+   }
+   
+   $('#search-entity').select2({
+      minimumInputLength: 2,
+      escapeMarkup: function (markup) {
+         return markup;
+      },
+      templateResult: function (data) {
+         return data.text;
+      },
+      templateSelection: function (data) {
+         return data.text;
+      },
+      ajax: {
+         url: function () { return conf.server + $('#type').val() + conf.suffix },
+         data: function (params) { return queryString (conf, params) },
+         headers: ajaxConf,
+         processResults: function (data) {
+            return {
+               results: process(data, $('#type').val())
+            };
          },
-         templateResult: function (data) {
-            return data.text;
-         },
-         templateSelection: function (data) {
-            return data.text;
-         },
-         ajax: {
-            url: function () { return conf.server + $('#type').val() + conf.suffix },
-            data: function (params) { return queryString (conf, params) },
-            headers: ajaxConf,
-            processResults: function (data) {
-               return {
-                  results: process(data, $('#type').val())
-               };
-            },
-            error: function ( jqXHR, textStatus, errorThrown ) {
-               handleError ( jqXHR, textStatus, errorThrown );
-            }
+         error: function ( jqXHR, textStatus, errorThrown ) {
+            handleError ( jqXHR, textStatus, errorThrown );
          }
-      });
+      }
+   });
 }
